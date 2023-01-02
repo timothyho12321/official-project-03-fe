778627f4716f7c965a5c51c87fe885de156a634c
timothyho12321 <timhotim2@gmail.com>
2023-01-02 10:28:13 +0000

    router initial add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/Home";
+import Product from "./pages/Products";
+
+
 import './App.css';
 
+
+
 function App() {
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/products">Soaps</Link>
+            </li>
+
+          </ul>
+        </nav>
+        <Routes>
+          {/* Home route */}
+          <Route path="/" element={<Home />
+          } />
+
+          {/* About Us route */}
+          <Route path="/about" element={<AboutUs />} />
+
+          {/* Products page */}
+          <Route path="/products" element={<Product />} />
+        </Routes>
+      </Router>
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Product() {
+    return (
+        <React.Fragment>
+            <h1>Products Page</h1>
+        </React.Fragment>
+
+    )
+}
+
