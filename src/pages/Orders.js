55fdb281024dfe2169d585fad48c484761cb9142
timothyho12321 <timhotim2@gmail.com>
2023-01-11 20:31:05 +0000

    added orders

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,12 +1,42 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 
-
 import axios from 'axios'
-import UserContext from '../contexts/UserContext'
-import { Button } from 'react-bootstrap'
+
+import { Button, Table } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
 export default function Orders() {
+
+    // const BASE_API_URL = "https://3000-timothyho12-officialpro-nd3lexqwq5u.ws-us81.gitpod.io/api/orders/"
+    const BASE_API_URL = "https://3000-timothyho12-officialpro-nd3lexqwq5u.ws-us82.gitpod.io/api/orders/"
+
+
+
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+
+        console.log("ran the useEffect")
+        const haveToken = JSON.parse(localStorage.getItem("currentUserTokens"))
+
+
+        async function findOrders() {
+            const response = await axios.get(BASE_API_URL, {
+                headers: {
+                    Authorization: `Bearer ${haveToken.accessToken}`
+                }
+            })
+            // console.log("response", response.data)
+            setOrders(response.data)
+        }
+
+        findOrders()
+
+
+
+    }, [])
+
+
 
 
 
@@ -14,7 +44,43 @@ export default function Orders() {
     return (
 
         <React.Fragment>
-            <h2>This is orders</h2>
+            <h2>Orders page</h2>
+
+            <Table bordered>
+                <thead>
+                    <tr>
+                        <td>
+                            id
+                        </td>
+                        <td>
+                            Account Id
+                        </td>
+                        <td>
+                            Total Cost
+                        </td>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {orders.map(o => (
+                        <tr>
+                            <td>
+                                {o.id}
+                            </td>
+                            <td>
+                                {o.account_id}
+                            </td>
+                            <td>
+                                {o.total_cost}
+                            </td>
+                        </tr>
+                    )
+                    )}
+                </tbody>
+
+            </Table>
+
 
 
         </React.Fragment>)
